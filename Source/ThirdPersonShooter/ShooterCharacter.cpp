ff7a21f334afaf1153c3618d21d26f73391b2212
Luis Ferreira <lg_pinguim@hotmail.com>
2021-08-31 21:23:38 -0300

    Created Camera Spring Arm.

--- Source/ThirdPersonShooter/ShooterCharacter.cpp
+++ Source/ThirdPersonShooter/ShooterCharacter.cpp
@@ -17,9 +17,32 @@ void AShooterCharacter::BeginPlay()
 	Super::BeginPlay();
 
 	//Log Examples
-	UE_LOG(LogTemp,Warning,TEXT("BeginPlay() Called"));
-	int myInt{42};
-	UE_LOG(LogTemp,Warning,TEXT("int myInt: %d"),myInt);
+	// UE_LOG(LogTemp,Warning,TEXT("BeginPlay() Called"));
+	// int myInt{42};
+	// UE_LOG(LogTemp,Warning,TEXT("int myInt: %d"),myInt);
+	//
+	// //Format Specifiers
+	// float myFloat{ 3.14159f};
+	// UE_LOG(LogTemp,Warning,TEXT("float myFloat: %f"),myFloat);
+	//
+	// double myDouble{0.000756};
+	// UE_LOG(LogTemp,Warning,TEXT("double myDouble: %lf"),myDouble);
+	//
+	// char myChar{'L'};
+	// UE_LOG(LogTemp,Warning,TEXT("char myChar: %c"),myChar);
+	//
+	// wchar_t wideChar{L'J'};
+	// UE_LOG(LogTemp,Warning,TEXT("wchar_t wideChar: %lc"),wideChar);
+	//
+	// bool myBool{true};
+	// UE_LOG(LogTemp,Warning,TEXT("bool myBool: %d"),myBool);
+	//
+	// UE_LOG(LogTemp,Warning,TEXT("int: %d, float: %f, bool: %d"),myInt,myFloat,false);
+	//
+	// FString myString{TEXT("My String")};
+	// UE_LOG(LogTemp,Warning,TEXT("FString myString: %s"),*myString);
+	//
+	// UE_LOG(LogTemp,Warning,TEXT("Name of Instance: %s"),*GetName());
 	
 }
 
