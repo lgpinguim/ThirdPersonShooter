2306082c0d49422920cedf0887a0457ac92d836e
Luis Ferreira <lg_pinguim@hotmail.com>
2021-09-15 20:20:55 -0300

    Added weapon fire sounds

--- Source/ThirdPersonShooter/ShooterCharacter.h
+++ Source/ThirdPersonShooter/ShooterCharacter.h
@@ -61,6 +61,9 @@ private:
 	/**Base look up/down rate, in deg/sec. Other scaling may affect turn rate*/
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Camera,meta = (AllowPrivateAccess = "true"))
 	float BaseLookUpRate;
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category = Combat, meta = (AllowPrivateAccess = "true"))
+	class USoundCue* FireSound;
 	
 public:
 	/**Returns CameraBoom subobject */
